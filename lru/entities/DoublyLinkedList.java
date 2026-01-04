c8be4c057efd79df34252a31c179cf28044456d8
sangam65 <sangambarnwal0823@gmail.com>
2026-01-04 12:15:07 +0530

    changes

--- lru/entities/DoublyLinkedList.java
+++ lru/entities/DoublyLinkedList.java
@@ -14,7 +14,7 @@ public class DoublyLinkedList<K, V> {
 
     }
 
-    public void addFirst(Node<K,V>node) throws NodeNullException{
+    public synchronized void addFirst(Node<K,V>node) throws NodeNullException{
         if(head==null||tail==null){
             throw new NodeNullException("head or tail node is null");
         }
@@ -30,7 +30,7 @@ public class DoublyLinkedList<K, V> {
 
     }
 
-    public void remove(Node<K, V> node)throws NodeNullException {
+    public synchronized void remove(Node<K, V> node)throws NodeNullException {
         if(node==null){
             throw new NodeNullException("Removal node can't be null");
         }
@@ -49,7 +49,7 @@ public class DoublyLinkedList<K, V> {
         addFirst(node);
     }
 
-    public Node<K, V> getLast() throws NodeNullException{
+    public  Node<K, V> getLast() throws NodeNullException{
         if(tail==null){
                throw new NodeNullException("tail node is null");
         }
@@ -59,7 +59,7 @@ public class DoublyLinkedList<K, V> {
         return null;
     }
 
-    public Node<K, V> createNode(K key, V value)throws RuntimeException {
+    public  synchronized Node<K, V> createNode(K key, V value)throws RuntimeException {
         if(key==null||value==null){
             throw new RuntimeException("Invalid input: key or value is invalid");
         }
