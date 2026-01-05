faeaf9351259033c8d2db456f78f455279ed675e
sangam65 <sangambarnwal0823@gmail.com>
2026-01-05 21:28:27 +0530

    integrated hashmap lld

--- hashmap/HashMapIntegration.java
+++ hashmap/HashMapIntegration.java
@@ -2,12 +2,12 @@ package hashmap;
 
 import hashmap.entities.HashMapImpl;
 
-public class HashMapImplementationClass {
-    public static void main(String []args){
+public class HashMapIntegration {
+     public static void main(String []args){
         HashMapImpl<Integer,Integer> hashMapImpl=new HashMapImpl<>();
         hashMapImpl.put(2, 5);
           System.out.println("For key 2"+hashMapImpl.getValue(2));
-        hashMapImpl.put(2,5);
+        hashMapImpl.put(2,4);
          System.out.println("For key 2"+hashMapImpl.getValue(2));
     }
 }
--- hashmap/entities/HashMapImpl.java
+++ hashmap/entities/HashMapImpl.java
@@ -1,25 +1,130 @@
 package hashmap.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import hashmap.exception.KeyException;
 
 public class HashMapImpl<K, V> {
-    private class Pair {
+   
+
+    private int capacity;
+    private int size;
+    private Pair<K,V> arrays[];
+
+    @SuppressWarnings("unchecked")
+    public HashMapImpl  () {
+        System.out.println("object creation through constructor");
+        this.capacity = 4;
+        this.size = 0;
+        this.arrays = new Pair[1<<this.capacity];
+        System.out.println("value {}"+this.arrays[1]);
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return 1 << capacity;
+    }
+    // private
+
+    public void put(K key, V value) {
+        Pair<K,V> oldPair = indexOfkeyExists(key);
+        if (oldPair != null) {
+
+            oldPair.setV(value);
+            return;
+        } else {
+
+            Pair<K,V> newPair = new Pair<K,V>(key, value);
+            int newInd = calculateKey(key);
+
+            if(arrays[newInd]==null){
+                arrays[newInd]=newPair;
+            }
+            else{
+                Pair<K,V> curPair=arrays[newInd];
+                while(curPair.getNext()!=null){
+                    curPair=curPair.next;
+                }
+                curPair.setNext(newPair);
+            }
+            this.size++;
+
+        }
+    }
+
+    public V getValue(K key) {
+        Pair<K,V> pair = indexOfkeyExists(key);
+        if (pair == null) {
+            throw new KeyException("key does not exist");
+        }
+
+        return pair.getV();
+    }
+
+    public void removeKey(K key) {
+        Pair<K,V> pair = indexOfkeyExists(key);
+        
+        if (pair == null) {
+            throw new KeyException("key does not exist");
+        }
+        int ind=calculateKey(key);
+        Pair<K,V> prevPair=arrays[ind];
+        if(prevPair==pair){
+            arrays[ind]=null;
+            return;
+        }
+        while(prevPair.next!=pair){
+            prevPair=prevPair.next;
+        }
+        prevPair.setNext(null);
+
+        // arrays.set(ind, null);
+
+        // have to remove this pair
+    }
+
+    private int calculateKey(K key) {
+        int totalCap = 1 << capacity;
+        int keyCode = key.hashCode();
+        int ind = keyCode % totalCap;
+        System.out.println("key "+key+" keyCode "+keyCode);
+        return ind;
+    }
+
+    private Pair<K,V> indexOfkeyExists(K key) {
+        int ind = calculateKey(key);
+
+        Pair<K,V> pair = arrays[ind];
+        while (pair != null) {
+            if (pair.getK().equals(key)) {
+                return pair;
+            }
+            pair = pair.getNext();
+
+        }
+        return null;
+    }
+
+
+     @SuppressWarnings("hiding")
+    private class Pair<K,V> {
         private final K k;
         private V v;
-        private Pair next;
+        private Pair<K,V> next;
 
         public K getK() {
             return k;
         }
 
-        public Pair getNext() {
+        public Pair<K,V> getNext() {
             return next;
         }
 
-        public void setNext(Pair next) {
+        public void setNext(Pair<K,V> next) {
             this.next = next;
         }
 
@@ -38,104 +143,4 @@ public class HashMapImpl<K, V> {
         }
 
     }
-
-    private int capacity;
-    private int size;
-    private List<Pair> arrays;
-
-    public HashMapImpl() {
-        System.out.println("object creation through constructor");
-        this.capacity = 4;
-        this.size = 0;
-        this.arrays = new ArrayList<>(1 << this.capacity);
-
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public int getCapacity() {
-        return 1 << capacity;
-    }
-    // private
-
-    public void put(K key, V value) {
-        Pair oldPair = indexOfkeyExists(key);
-        if (oldPair != null) {
-
-            oldPair.setV(value);
-            return;
-        } else {
-
-            Pair newPair = new Pair(key, value);
-            int newInd = calculateKey(key);
-
-            if(arrays.get(newInd)==null){
-                arrays.set(newInd, newPair);
-            }
-            else{
-                Pair curPair=arrays.get(newInd);
-                while(curPair.getNext()!=null){
-                    curPair=curPair.next;
-                }
-                curPair.setNext(newPair);
-            }
-            this.size++;
-
-        }
-    }
-
-    public V getValue(K key) {
-        Pair pair = indexOfkeyExists(key);
-        if (pair == null) {
-            throw new KeyException("key does not exist");
-        }
-
-        return pair.getV();
-    }
-
-    public void removeKey(K key) {
-        Pair pair = indexOfkeyExists(key);
-        
-        if (pair == null) {
-            throw new KeyException("key does not exist");
-        }
-        int ind=calculateKey(key);
-        Pair prevPair=arrays.get(ind);
-        if(prevPair==pair){
-            arrays.set(ind, null);
-            return;
-        }
-        while(prevPair.next!=pair){
-            prevPair=prevPair.next;
-        }
-        prevPair.setNext(null);
-
-        // arrays.set(ind, null);
-
-        // have to remove this pair
-    }
-
-    private int calculateKey(K key) {
-        int totalCap = 1 << capacity;
-        int keyCode = key.hashCode();
-        int ind = keyCode % totalCap;
-        return ind;
-    }
-
-    private Pair indexOfkeyExists(K key) {
-        int ind = calculateKey(key);
-
-        Pair pair = arrays.get(ind);
-        while (pair != null) {
-            if (pair.getK().equals(key)) {
-                return pair;
-            }
-            pair = pair.getNext();
-
-        }
-        return null;
-    }
-
 }
