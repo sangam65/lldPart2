5c299c8b26bc0370c804deb9a8fe0c97904348d5
sangam65 <sangambarnwal0823@gmail.com>
2026-01-05 21:28:49 +0530

    fixed changs i LRU

--- lru/LRUIntegration.java
+++ lru/LRUIntegration.java
@@ -3,7 +3,7 @@ package lru;
 import lru.entities.LRU;
 
 public class LRUIntegration {
-   `
+    public static void main(String []args){
         LRU<Integer,String> lru=new LRU<>(5);
         lru.addValue(3, "sangam");
         lru.addValue(4, "rahul");
